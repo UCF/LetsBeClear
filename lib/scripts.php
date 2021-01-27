87a1492b7fecb9a366d19b3e06c6247327fc4816
Vinnie Vu <vinhtvu2@gmail.com>
2021-01-27 14:22:35 -0500

    Update Jquery fix for Letsbeclear

--- lib/scripts.php
+++ lib/scripts.php
@@ -26,10 +26,10 @@ function roots_scripts() {
   // jQuery is loaded in header.php using the same method from HTML5 Boilerplate:
   // Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline
   // It's kept in the header instead of footer to avoid conflicts with plugins.
-  if (!is_admin()) {
-    wp_deregister_script('jquery');
-    wp_register_script('jquery', '', '', '1.11.2', false);
-  }
+  // if (!is_admin()) {
+  //  wp_deregister_script('jquery');
+  //  wp_register_script('jquery', '', '', '1.11.2', false);
+  // }
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
